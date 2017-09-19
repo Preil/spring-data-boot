96ef5d6d32b33110418ab8ecbf90ccdf3bb2cbaa
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-09-19 08:41:04 +0300

    Application modified to findOne book.

--- src/main/java/com/oreilly/sdata/Application.java
+++ src/main/java/com/oreilly/sdata/Application.java
@@ -2,9 +2,6 @@ package com.oreilly.sdata;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 /**
  * Ilya 18.09.2017.
  */
@@ -12,8 +9,8 @@ public class Application {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-        BookService service = context.getBean(BookService.class);
-        Book book = new Book("Catcher in the Rye", new Date(), 250, new BigDecimal("15.00"));
-        service.save(book);
+        BookRepository repository = context.getBean(BookRepository.class);
+        Book book = repository.findOne(1L);
+        System.out.println(book);
     }
 }
