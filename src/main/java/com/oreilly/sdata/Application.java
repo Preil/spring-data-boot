42622f05cbfdd27abdb698ef3b481d5a83f9c253
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-09-19 08:43:26 +0300

    Retrieving all books.

--- src/main/java/com/oreilly/sdata/Application.java
+++ src/main/java/com/oreilly/sdata/Application.java
@@ -2,6 +2,8 @@ package com.oreilly.sdata;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 /**
  * Ilya 18.09.2017.
  */
@@ -10,7 +12,10 @@ public class Application {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
         BookRepository repository = context.getBean(BookRepository.class);
-        Book book = repository.findOne(1L);
-        System.out.println(book);
+
+        List<Book> books = repository.findAll();
+        for(Book book:books){
+            System.out.println(book);
+        }
     }
 }
